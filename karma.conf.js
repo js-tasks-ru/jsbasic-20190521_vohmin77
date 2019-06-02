073187704374f1c456138043e7599e2acdc176a1
Burlak Ilia <i.l.burlak@gmail.com>
2019-06-02 10:27:04 +0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration
 // Generated on Sun Nov 26 2017 12:48:45 GMT+0000 (GMT)
+const { TRAVIS } = process.env;
 
 module.exports = function(config) {
   config.set({
@@ -34,7 +35,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['json'], // progress
+    reporters: [TRAVIS ? 'json' : 'mocha'], // progress
     
     jsonReporter: {
       stdout: true,
@@ -50,7 +51,7 @@ module.exports = function(config) {
     
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DISABLE,
+    logLevel: TRAVIS ? config.LOG_INFO : config.LOG_ERROR,
     
     browserConsoleLogOptions: {
       terminal: false
