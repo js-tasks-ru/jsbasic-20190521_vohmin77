247d16e7e581ab4c155ab0cca6ebc988944f9fbe
Roman Vokhmin <vohmin77@gmail.com>
2019-06-14 15:08:40 +0600

    Решена задача по выборке из массива объектов

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,6 +5,16 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let result = '';
+  for (let i = 0; i < data.length; i += 1) {
+    for (let key in data[i]) {
+      if (data[i].age <= age) {
+        result += (`${data[i].name}, ${data[i].balance}\n`);
+      }
+      break;
+    }
+  }
+  return result.slice(0, result.length - 1);
 }
 
 
